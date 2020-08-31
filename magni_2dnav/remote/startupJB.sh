1f46b0d863e5103ea056470c7238acce652d2899
Magni <mcontr15@ucsc.edu>
2020-08-31 14:45:59 -0700

    edited JB startup script

--- magni_2dnav/remote/startupJB.sh
+++ magni_2dnav/remote/startupJB.sh
@@ -1,6 +1,6 @@
-python check_ip_JR.py
+python check_ip_JB.py
 
-parallel -u ::: 'rosrun magni_2dnav startupGUI2.py' 'roslaunch magni_2dnav remote_start_JR.launch'
+parallel -u ::: 'rosrun magni_2dnav startupGUI2.py' 'roslaunch magni_2dnav remote_start_JB.launch'
 
 #xterm -e rosrun magni_2dnav startupGUI.py 
 #xterm -e roslaunch magni_2dnav remote_start.launch
